fa7bd25a8528f3904c9d8f0461a70d2fbf4f08e1
Aditya Yada <adityayada17@gmail.com>
2023-03-06 01:25:01 +0530

    VKFYR-11 Business-level operations: 4.Navigation option to close the current execution context and return to the main context

--- src/com/lcme/app/LockedMeMain.java
+++ src/com/lcme/app/LockedMeMain.java
@@ -9,8 +9,10 @@ public class LockedMeMain {
     public static void main(String[] args) {
         LockedMeFileOps.init();
         System.out.println(LockedmeConstants.APPLICATION_INFO);
-        System.out.println(LockedmeConstants.MAIN_MENU);
         Scanner sc = new Scanner(System.in);
+        boolean runMainMenu = true;
+        while(runMainMenu){
+        System.out.println(LockedmeConstants.MAIN_MENU);
         if(sc.hasNext()){
             String option = sc.next();
             sc.nextLine();
@@ -20,39 +22,42 @@ public class LockedMeMain {
                     break;
                 }
                 case "2" : {
-                    System.out.println(LockedmeConstants.BUSINESS_MENU);
-                    if(sc.hasNext()){
-                    option = sc.next();
-                    sc.nextLine();
-                    switch (option){
-                        case "1" : {
-                            System.out.println("Enter file name with relative path");
-                            String fileNameWithRelativePath = sc.next();
-                            System.out.println("Enter file contents");
-                            String fileContent = sc.next();
-                            LockedMeFileOps.addFile(fileNameWithRelativePath,fileContent);
-                            break;
+                    boolean runBusinessmenu = true;
+                    while(runBusinessmenu){
+                        System.out.println(LockedmeConstants.BUSINESS_MENU);
+                        if(sc.hasNext()){
+                            option = sc.next();
+                            sc.nextLine();
+                            switch (option){
+                                case "1" : {
+                                    System.out.println("Enter file name with relative path");
+                                    String fileNameWithRelativePath = sc.next();
+                                    System.out.println("Enter file contents");
+                                    String fileContent = sc.next();
+                                    LockedMeFileOps.addFile(fileNameWithRelativePath,fileContent);
+                                    break;
+                                }
+                                case "2" : {
+                                    System.out.println("Enter file name with relative path");
+                                    String fileNameWithRelativePath = sc.next();
+                                    LockedMeFileOps.deleteFile(fileNameWithRelativePath);
+                                    break;
+                                }
+                                case "3" : {
+                                    System.out.println("Enter file name");
+                                    String fileName = sc.next();
+                                    LockedMeFileOps.searchFile(fileName);
+                                    break;
+                                }
+                                case "4" : {
+                                    runBusinessmenu = false;
+                                    break;
+                                }
+                                default : {
+                                    System.out.println("Invalid Input");
+                                }
+                            }
                         }
-                        case "2" : {
-                            System.out.println("Enter file name with relative path");
-                            String fileNameWithRelativePath = sc.next();
-                            LockedMeFileOps.deleteFile(fileNameWithRelativePath);
-                            break;
-                        }
-                        case "3" : {
-                            System.out.println("Enter file name");
-                            String fileName = sc.next();
-                            LockedMeFileOps.searchFile(fileName);
-                            break;
-                        }
-                        case "4" : {
-                            System.out.println("Navigating back to main menu");
-                            break;
-                        }
-                        default : {
-                            System.out.println("Invalid Input");
-                        }
-                    }
                     }
                     break;
                 }
@@ -61,6 +66,9 @@ public class LockedMeMain {
                 }
             }
         }
+
+        }
         sc.close();
+        System.out.println("Closing the application...");
     }
 }
