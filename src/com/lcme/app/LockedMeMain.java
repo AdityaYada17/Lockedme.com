828e4f2c99cb734459de97ae9806e6efd98fdc81
Aditya Yada <adityayada17@gmail.com>
2023-03-05 22:06:39 +0530

    VKFYR-12 Welcome Screen: Display Application name and the developer details

---
+++ src/com/lcme/app/LockedMeMain.java
@@ -0,0 +1,9 @@
+package com.lcme.app;
+
+import java.util.Scanner;
+
+public class LockedMeMain {
+    public static void main(String[] args) {
+        System.out.println(LockedmeConstants.APPLICATION_INFO);
+    }
+}
---
+++ src/com/lcme/app/LockedmeConstants.java
@@ -0,0 +1,17 @@
+package com.lcme.app;
+
+public class LockedmeConstants {
+
+    public static final String APPLICATION_INFO =
+            "#############################################################################" + System.lineSeparator() +
+            "#***************************************************************************#" + System.lineSeparator() +
+            "#*******************#####################################*******************#" + System.lineSeparator() +
+            "#*******************#Virtual Key for Your Repositories  #*******************#" + System.lineSeparator() +
+            "#*******************#Company Lockers Pvt. Ltd           #*******************#" + System.lineSeparator() +
+            "#*******************#Lockedme.com                       #*******************#" + System.lineSeparator() +
+            "#*******************#Aditya Yada                        #*******************#" + System.lineSeparator() +
+            "#*******************#FULL Stack Developer               #*******************#" + System.lineSeparator() +
+            "#*******************#####################################*******************#" + System.lineSeparator() +
+            "#***************************************************************************#" + System.lineSeparator() +
+            "#############################################################################";
+}
