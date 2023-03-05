7fa9022f890d67d15fdedf081666a70c96fb4f25
Aditya Yada <adityayada17@gmail.com>
2023-03-05 22:58:39 +0530

    VKFYR-6 Welcome Screen: 2.Display Business-level operations

--- src/com/lcme/app/LockedMeMain.java
+++ src/com/lcme/app/LockedMeMain.java
@@ -1,19 +1,50 @@
 package com.lcme.app;
 
+import com.sun.source.tree.SwitchTree;
+
+import java.lang.invoke.SwitchPoint;
 import java.util.Scanner;
 
 public class LockedMeMain {
     public static void main(String[] args) {
-
         LockedMeFileOps.init();
         System.out.println(LockedmeConstants.APPLICATION_INFO);
         System.out.println(LockedmeConstants.MAIN_MENU);
         Scanner sc = new Scanner(System.in);
         if(sc.hasNext()){
             String option = sc.next();
+            sc.nextLine();
             switch (option){
                 case "1" : {
                     LockedMeFileOps.retrieveAllFileNamesInRootAsc(LockedMeFileOps.root);
+                    break;
+                }
+                case "2" : {
+                    System.out.println(LockedmeConstants.BUSINESS_MENU);
+                    if(sc.hasNext()){
+                    option = sc.next();
+                    switch (option){
+                        case "1" : {
+                            System.out.println("Adding a file");
+                            break;
+                        }
+                        case "2" : {
+                            System.out.println("Deleting a file");
+                            break;
+                        }
+                        case "3" : {
+                            System.out.println("Searching for a file");
+                            break;
+                        }
+                        case "4" : {
+                            System.out.println("Navigating back to main menu");
+                            break;
+                        }
+                        default : {
+                            System.out.println("Invalid Input");
+                        }
+                    }
+                    }
                     break;
                 }
                 default :  {
--- src/com/lcme/app/LockedmeConstants.java
+++ src/com/lcme/app/LockedmeConstants.java
@@ -17,5 +17,13 @@ public class LockedmeConstants {
 
     public static String MAIN_MENU =
             "Please choose an option from below main menu:" + System.lineSeparator() +
-            "1.Retrieving the file names in an ascending order" + System.lineSeparator();
+            "1.Retrieving the file names in an ascending order" + System.lineSeparator() +
+            "2.Business Operations";
+
+    public static String BUSINESS_MENU =
+            "Please choose an option from below business menu: " + System.lineSeparator() +
+            "1.Add a file" + System.lineSeparator() +
+            "2.Delete a file" + System.lineSeparator() +
+            "3.Search for a file" + System.lineSeparator() +
+            "4.Navigate back to main menu";
 }
